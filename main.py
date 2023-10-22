99a37b602615757ebc7dd661f597e02ccbdfc7ee
ayoubim <ayoubim@berea.edu>
2023-10-22 03:22:40 -0400

    fixed some bugs

--- main.py
+++ main.py
@@ -25,7 +25,14 @@ def stock_price_checker():
     yesterday_closing_price = float(first_2_items[0][1]["4. close"])
     day_before_yesterday_closing_price = float(first_2_items[1][1]["4. close"])
     difference = (yesterday_closing_price / day_before_yesterday_closing_price) - 1
-    return round(difference, 2)
+    return round(difference, 3)
+
+
+def symbol_retriever():
+    if stock_price_checker() > 0:
+        return "🔺"
+    else:
+        return "🔻"
 
 
 ## STEP 2: Use https://newsapi.org
@@ -48,39 +55,23 @@ def news_retriever():
 # Send a seperate message with the percentage change and each article's title and description to your phone number.
 
 
-def symbol_retriever():
-    if stock_price_checker() > 0:
-        return "🔺"
-    else:
-        return "🔻"
-
-
 def send_message():
-    if abs(stock_price_checker()) > 0.01:
+    if abs(stock_price_checker()) > 0.05:
+
         account_sid = os.environ['TWILIO_ACCOUNT_SID']
         auth_token = os.environ['TWILIO_AUTH_TOKEN']
         client = Client(account_sid, auth_token)
-
-        message = client.messages \
-            .create(
-            body=f"{STOCK}: {symbol_retriever()}{stock_price_checker()}\n Headline: {news_retriever()[0]['title']} \n "
-                 f"Brief: {news_retriever()[0]['description']}",
-            from_='+18554588334',
-            to='+18599799101',
-        )
-
-        print(message.status)
+        n = 0
+        for article in news_retriever():
+            message = client.messages \
+                .create(
+                body=f"{STOCK}: {symbol_retriever()}{stock_price_checker()}\n"
+                     f" Headline: {news_retriever()[n]['title']}\nBrif: {news_retriever()[n]['description']}",
+                from_='+18554588334',
+                to='+18599799101',
+            )
+            n += 1
+            print(message.status)
 
 
 send_message()
-
-# Optional: Format the SMS message like this:
-"""
-TSLA: 🔺2%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-or
-"TSLA: 🔻5%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-"""
